b5800add96de51c4eec485dc035288258d8eae64
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2026-02-21 15:05:34 -0500

    instructing ur mom

--- INSTRUCTION_MEMORY.v
+++ INSTRUCTION_MEMORY.v
@@ -1,6 +1,11 @@
+// reads from instruction memory and outputs the instruction
 module INSTRUCTION_MEMORY (
     input iRdAddr,
     output [31:0] oInstr
 );
+    // 500 instructions (will need to update based on autograder)
+    // each instruction is 32 bits
+    reg [31:0] memory [0:501];
+    assign oInstr = memory[iRdAddr[31:2]]; // need to divide by 4
     
 endmodule
