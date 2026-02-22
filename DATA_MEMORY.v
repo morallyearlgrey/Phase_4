04eca678831fb3f4bc74b3aaa4346ca13dc3f216
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2026-02-21 20:42:13 -0500

    datadatadatadata

--- DATA_MEMORY.v
+++ DATA_MEMORY.v
@@ -9,43 +9,67 @@ module DATA_MEMORY (
     output reg [31:0] oReadData
 );
 
-    reg [31:0] memory [0:501];
+    reg [7:0] memory [0:1999]; // 8 bits=500 words
     // read logic
     // combinational logic, determined by current input without past inputs
-    // 
     always @(*) begin
         if(iMemRead == 1) begin
-            case iFunct3 // take a look and return data sign extended too
-                // lb
+            case iFunct3 // changes oreaddata depending on funct3
+                // load byte, signextend
                 3'b000:
-                    assign oReadData = {24{memory[iAddress][7]}, memory[iAddress]};
+                    oReadData = {{24{memory[iAddress+0][7]}, memory[iAddress+0]}};
 
-                // lh
+                // load halfword, signextend
                 3'b001:
-                    assign oReadData = {16{memory[iAddress+1][15]}, memory[iAddress+1]};
+                    oReadData = {{16{memory[iAddress+1][7]}}, memory[iAddress+1], memory[iAddress+0]};
 
-                // lw
+                // load word
                 3'b010:
-                    assign oReadData = {16{memory[iAddress][15]}, memory[iAddress]};
+                    oReadData = {memory[iAddress+3], memory[iAddress+2], memory[iAddress+1], memory[iAddress+0]};
 
-                // lbu
+                // load byte unsigned, zeroextend
                 3'b100:
+                    oReadData = {24'b0, memory[iAddress]};
 
-                // lhu
+                // load halfword unsigned, zeroextend
                 3'b101:
+                    oReadData = {16'b0, memory[iAddress+1], memory[iAddress+0]};
+
+                default:
+                    oReadData = 32'b0;
 
             endcase
             
         end else begin 
 
-
         end    
     end
 
-
-
     // write logic
+    // in phase 3 wasn't combinational, but no clock here so
+    always @(*) begin
+        if(iMemWrite == 1) begin
+            case (iFunct3)
+                // store byte
+                3'b000:
+                    mem[iAddress+0] = iWriteData[7:0];
 
+                // store half-word
+                3'b001:
+                    mem[iAddress+0] = iWriteData[7:0];
+                    mem[iAddress+1] = iWriteData[15:8];
 
-    
+                // store word
+                3'b010:
+                    mem[iAddress+0] = iWriteData[7:0];
+                    mem[iAddress+1] = iWriteData[15:8];
+                    mem[iAddress+2] = iWriteData[23:16];
+                    mem[iAddress+3] = iWriteData[31:24];
+
+            endcase
+
+        end
+
+    end
+
 endmodule
--- INSTRUCTION_MEMORY.v
+++ INSTRUCTION_MEMORY.v
@@ -5,7 +5,7 @@ module INSTRUCTION_MEMORY (
 );
     // 500 instructions (will need to update based on autograder)
     // each instruction is 32 bits
-    reg [31:0] memory [0:501];
+    reg [31:0] memory [0:499];
     assign oInstr = memory[iRdAddr[31:2]]; // need to divide by 4
     
 endmodule
