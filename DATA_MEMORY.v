82bbf6547dfbbe88cf499376802ee745da1eb325
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2026-02-25 23:16:32 -0500

    whoops

--- DATA_MEMORY.v
+++ DATA_MEMORY.v
@@ -16,25 +16,30 @@ module DATA_MEMORY (
         if(iMemRead == 1) begin
             case iFunct3 // changes oreaddata depending on funct3
                 // load byte, signextend
-                3'b000:
-                    oReadData = {{24{memory[iAddress+0][7]}, memory[iAddress+0]}};
-
+                3'b000: begin
+                    oReadData = {{24{memory[iAddress][7]}}, memory[iAddress]};
+                
+                end
                 // load halfword, signextend
-                3'b001:
+                3'b001: begin
                     oReadData = {{16{memory[iAddress+1][7]}}, memory[iAddress+1], memory[iAddress+0]};
 
+                end
                 // load word
-                3'b010:
+                3'b010: begin
                     oReadData = {memory[iAddress+3], memory[iAddress+2], memory[iAddress+1], memory[iAddress+0]};
 
+                end
                 // load byte unsigned, zeroextend
-                3'b100:
+                3'b100: begin
                     oReadData = {24'b0, memory[iAddress]};
 
+                end
                 // load halfword unsigned, zeroextend
-                3'b101:
+                3'b101: begin
                     oReadData = {16'b0, memory[iAddress+1], memory[iAddress+0]};
 
+                end
                 default:
                     oReadData = 32'b0;
 
@@ -52,21 +57,24 @@ module DATA_MEMORY (
         if(iMemWrite == 1) begin
             case (iFunct3)
                 // store byte
-                3'b000:
-                    mem[iAddress+0] = iWriteData[7:0];
+                3'b000: begin
+                    memory[iAddress+0] = iWriteData[7:0];
 
+                end
                 // store half-word
-                3'b001:
-                    mem[iAddress+0] = iWriteData[7:0];
-                    mem[iAddress+1] = iWriteData[15:8];
+                3'b001: begin
+                    memory[iAddress+0] = iWriteData[7:0];
+                    memory[iAddress+1] = iWriteData[15:8];
 
+                end
                 // store word
-                3'b010:
-                    mem[iAddress+0] = iWriteData[7:0];
-                    mem[iAddress+1] = iWriteData[15:8];
-                    mem[iAddress+2] = iWriteData[23:16];
-                    mem[iAddress+3] = iWriteData[31:24];
-
+                3'b010: begin
+                    memory[iAddress+0] = iWriteData[7:0];
+                    memory[iAddress+1] = iWriteData[15:8];
+                    memory[iAddress+2] = iWriteData[23:16];
+                    memory[iAddress+3] = iWriteData[31:24];
+                end
+                
             endcase
 
         end
